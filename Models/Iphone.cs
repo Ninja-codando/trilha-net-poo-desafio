d7d086d36381f77dccd5ab5c49ee73408bf7dbdc
Raphael Lopes <Rapha1.dev@gmail.com>
2025-09-15 08:43:16 -0300

    Desafio Concluído!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        // Construtor (opcional, mas recomendado se a classe pai exigir parâmetros)
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando aplicativo {nome} no iPhone via App Store.");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        // Construtor (opcional, mas recomendado se a classe pai exigir parâmetros)
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando aplicativo {nome} no Nokia via loja da Microsoft.");
+        }
     }
 }
